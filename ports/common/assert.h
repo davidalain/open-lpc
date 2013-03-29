955e44c82e06e7b71c85bba687469eaf59aeb508
Cristóvão Rufino <cristovaozr@gmail.com>
2013-03-29 17:36:29 -0300

    Alterações pequenas

---
+++ ports/common/assert.h
@@ -0,0 +1,14 @@
+/**
+ * assert.h
+ * Função para verificação de código-fonte
+ * Autor: Cristóvão Zuppardo Rufino <cristovaozr@gmail.com>
+ * 29/03/2013
+ */
+
+#ifndef ASSERT_H
+#define ASSERT_H
+
+inline void assert (const char *msg, uint32_t exp);
+
+#endif
+
